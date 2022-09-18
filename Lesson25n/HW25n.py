6c98981b3a1827913abeead7bde7816eadab9eab
MikeKorsikov <nikolay.korsikov@gmail.com>
2022-09-18 11:15:17 +0200

    HW on pizza decorators

--- Lesson25n/HW25n.py
+++ Lesson25n/HW25n.py
@@ -1,1 +1,57 @@
-print('Exercise Pizza:')
+print('Exercise Pizza:\n')
+
+class Pizza1:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        print('Name: Margherita')
+        print('Ingredients: basil, mozzarella, and tomato.')
+        return self.func(*args)
+
+
+class Pizza2:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        print('Name: Four Cheeses')
+        print('Ingredients: tomato sauce, mozzarella, gorgonzola, '
+              'Parmigiano Reggiano, and goat cheese.')
+        return self.func(*args)
+
+
+class Pizza3:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        print('Name: Capricciosa')
+        print('Ingredients: mozzarella cheese, Italian baked ham, '
+              'mushroom, artichoke and tomato.')
+        return self.func(*args)
+
+
+class Pizza4:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        print('Name: Hawaiian')
+        print('Ingredients: pizza sauce, cheese, cooked ham, and pineapple.')
+        return self.func(*args)
+
+
+# function for ordering pizza
+def orderPizza(quantity):
+    print(f"{quantity} pizza ordered:")
+
+    @Pizza3
+    def bakePizza():
+        print('[Pizza is baking...]')
+
+    bakePizza()
+
+
+# invoking
+orderPizza(1)
